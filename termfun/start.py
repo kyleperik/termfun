2e78fea5c6e7f39b77760d9bd50c5d6d71edc795
kyleperik <kyle@periks.net>
2018-04-29 11:01:42 -0500

    fix indentation

--- termfun/start.py
+++ termfun/start.py
@@ -4,16 +4,16 @@ term = Terminal()
 
 # cred https://gist.github.com/jasonrdsouza/1901709
 def getchar():
-   #Returns a single character from standard input
-   import tty, termios, sys
-   fd = sys.stdin.fileno()
-   old_settings = termios.tcgetattr(fd)
-   try:
-      tty.setraw(sys.stdin.fileno())
-      ch = sys.stdin.read(1)
-   finally:
-      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-   return ch
+    #Returns a single character from standard input
+    import tty, termios, sys
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 def draw(x, y, v, color=7, bgcolor=None):
     s = str(v)
