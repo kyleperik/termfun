702562c4b8938e753198fcd500bb59086f440f3e
Kyle <kyle@periks.net>
2019-10-28 18:54:37 -0500

    fix render timing

--- termfun/main.py
+++ termfun/main.py
@@ -19,7 +19,6 @@ def draw(x, y, v, color=7, bgcolor=None):
 
 def main(state, render, step, skip, fps, i):
     print(term.clear)
-    render(state)
     c = (i.escape_code() if not skip(state) else ' ') if fps is None else i.char()
     # Clear the rest of the chars queued, if any
     while i.char(): pass
@@ -27,6 +26,7 @@ def main(state, render, step, skip, fps, i):
         return
     nextstate = step(state, c or ' ')
     if nextstate is None: return
+    render(nextstate)
     if fps: sleep(60 / fps / 100)
     return lambda: main(nextstate, render, step, skip, fps, i)
 
